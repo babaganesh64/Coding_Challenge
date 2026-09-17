1c4c6a3ca77927d42a2b2b546851707299516ceb
Baba Ganesh <babaganesh6445u@gmail.com>
2026-09-17 11:22:26 +0530

    Added ThresholdQuarantine problem statement with incompleted logic

---
+++ Vectors/ThresholdQuarantine.java
@@ -0,0 +1,19 @@
+/*  Scenario: A load balancer holds a queue of server response times (in milliseconds). 
+    Any response time greater than 100 is classified as a "lag spike". 
+    Your algorithm must extract all lag spikes from the primary queue and move them into a separate "Quarantine" queue.
+   
+    Input Primary Vector: [45, 120, 88, 205, 15, 99, 101]
+    Expected Primary Vector State: [45, 88, 15, 99]
+    Expected Quarantine Vector State: [120, 205, 101]
+    
+    Constraint: When a lag spike is found, you must dynamically .add() it to the Quarantine Vector and immediately .remove() it from the Primary Vector. */
+
+package Vectors;
+
+import java.util.Vector;
+
+public class ThresholdQuarantine {
+    int[] primaryarray = {45, 120, 88, 205, 15, 99, 101};
+    Vector<Integer> Primary = new Vector<>();
+
+}
