c2012323b898a149181bfc8aba0aa93644d5cb10
Baba Ganesh <babaganesh6445u@gmail.com>
2026-09-24 15:51:52 +0530

    Added BadSectorPurge problem statement in vectors

---
+++ Vectors/BadSectorPurge.java
@@ -0,0 +1,17 @@
+/*  Concept be learnt by this problem : Range Deletion
+    Scenario: A file downloading client receives data packets. A parity check reveals that a sector 
+    of memory got corrupted during transmission. You must write an algorithm that deletes a specific 
+    range of indices—specifically, you must purge exactly three elements starting from index 2.
+   
+    Input Vector: [10, 20, 999, 999, 999, 60, 70]
+    Expected Vector State: [10, 20, 60, 70]
+    Constraint: You must write a loop that executes exactly 3 times, using .remove() to slice out 
+    the bad sector. Watch the shifting trap! */
+
+package Vectors;
+
+public class BadSectorPurge {
+    public static void main(String[] args){
+        
+    }
+}
